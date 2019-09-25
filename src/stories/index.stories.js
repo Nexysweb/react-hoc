2ab55e174b187bc68beb30f30ff42df758c32e83
John B <john@nexys.ch>
2019-09-25 23:15:44 +0200

    added numeric filter

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -10,19 +10,22 @@ import Nav from '../components/nav'
 import List from '../components/list'
 
 const data = [
-  {name: 'Sheep', location: 'Europe', country: {name: 'United Kingdom'}, amount: 23},
-  {name: 'Tiger', location: 'Asia', country: {name: 'India'}, amount: 43},
-  {name: 'Elephant', location: 'Africa', country: {name: 'Tanzania'}, amount: 3},
-  {name: 'Lion', location: 'Africa', country: {name: 'South Africa'}, amount: 2},
-  {name: 'Cat', location: 'Europe', country: {name: 'Germany'}, amount: 7},
-  {name: 'Grizzly', location: 'America', country: {name: 'Canada'}, amount: 43},
-  {name: 'Antelope', location: 'Africa', country: {name: 'Namibia'}, amount: 87}];
+  {name: 'Sheep', location: 'Europe', country: {name: 'United Kingdom'}, amount: 23.3, int: 23, date: '2019-09-05'},
+  {name: 'Tiger', location: 'Asia', country: {name: 'India'}, amount: 24.1, int:  43, date: '2019-09-05'},
+  {name: 'Elephant', location: 'Africa', country: {name: 'Tanzania'}, amount: 23, int:  3, date: '2019-09-05'},
+  {name: 'Lion', location: 'Africa', country: {name: 'South Africa'}, amount: 0.3, int:  2, date: '2019-09-05'},
+  {name: 'Cat', location: 'Europe', country: {name: 'Germany'}, amount: 2.31, int:  7, date: '2019-09-05'},
+  {name: 'Grizzly', location: 'America', country: {name: 'Canada'}, amount: 3.35, int:  43, date: '2019-09-05'},
+  {name: 'Antelope', location: 'Africa', country: {name: 'Namibia'}, amount: 2.3, int:  87, date: '2019-09-05'}
+];
 
 const def = [
   {name: 'name'},
   {name: 'location'},
   {name: 'country.name', label: 'Country'},
-  {name: 'amount', label: 'A long label'}
+  {name: 'amount', label: 'A long label'},
+  {name: 'int', label: 'd'},
+  {name: 'date', label: 'a date'}
 ];
 
 const nav = [{name: 'Name', link: '/sdf/sdf'}]
