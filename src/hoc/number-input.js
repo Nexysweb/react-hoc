53b4c5a4b9df59966aa0ee4c79c9b0e03a4841b0
John B <john@nexys.ch>
2019-11-21 10:55:56 +0100

    updated input numeric components

--- src/hoc/number-input.js
+++ src/hoc/number-input.js
@@ -66,33 +66,31 @@ const wrapComponent = () => WrappedComponent => class Hoc extends React.Componen
     };
   }
 
-  
   handleChange = (event) => {
-    // get new value
-    let newValue = event.target.rawValue;
+    // get raw numeric value
+    const value = Number(event.target.rawValue);
+    const { precision } = this.state;
 
-    // update state
-    this.setState({value: newValue});
+    // update state with formatted value
+    this.setState({value: formatNumber(value, precision)});
 
-    // calls parent function
-    this.props.onChange({name: this.state.name, value: Number(newValue)});
+    // pass numeruc value to parent onChange function
+    this.props.onChange({name: this.state.name, value});
   }
 
-  handleBlur = () => {
-    const value = formatNumber(this.state.value, this.state.precision);
-    this.setState({value});
+  handleBlur = event => {
+    // get raw numeric value
+    const value = Number(event.target.rawValue);
+    const { precision } = this.state;
 
+    // update state with formatted value
+    this.setState({value: formatNumber(value, precision)});
+
+    // if parent onBlur, pass the numeric value
     if (typeof this.props.onBlur !== 'undefined') {
-      this.props.onBlur({name: this.state.name, value: Number(this.state.value)});
+      this.props.onBlur({name: this.state.name, value});
     }
   }
-
-  // props and state collision
-  // componentDidUpdate(nextProps) {
-  //   if (nextProps.value !== this.props.value) {
-  //     this.processProps(nextProps);
-  //   }
-  // }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.value !== this.props.value) {
@@ -114,15 +112,15 @@ const wrapComponent = () => WrappedComponent => class Hoc extends React.Componen
 
     return (
       <WrappedComponent
-          value={this.state.value}
-          className={className}
-          options={options}
-          onChange={this.handleChange}
-          onBlur={this.handleBlur}
-          placeholder={this.props.placeholder}
-          disabled={this.state.disabled ? true : undefined}
-          style={style}
-          suffix={this.state.suffix}
+        value={this.state.value}
+        className={className}
+        options={options}
+        onChange={this.handleChange}
+        onBlur={this.handleBlur}
+        placeholder={this.props.placeholder}
+        disabled={this.state.disabled ? true : undefined}
+        style={style}
+        suffix={this.state.suffix}
       />
     );
   }
