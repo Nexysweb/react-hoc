5296a1b561c3ad9de6575ddc4256ba4cec9a45be
John B <john@nexys.ch>
2019-11-24 22:39:40 +0100

    storybook

---
+++ docs/precache-manifest.758e523c1f4147f67625eb960c90ec0a.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1572115be969eb2c594caad3471749e2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.aabf8934162c028ea7b5.bundle.js"
+  },
+  {
+    "revision": "5cf622bea269f97fd040",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.aabf8934162c028ea7b5.bundle.js"
+  },
+  {
+    "url": "vendors~main.aabf8934162c028ea7b5.bundle.js"
+  },
+  {
+    "revision": "04f21ddfe0ab0cd00835",
+    "url": "vendors~main.css"
+  }
+]);
