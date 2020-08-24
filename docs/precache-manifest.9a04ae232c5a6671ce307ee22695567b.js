1677c7ff778eb527e47d7bde57323706c12f954f
Johan <johan@macair2020>
2020-08-24 19:46:04 +0200

    publish Storybook

---
+++ docs/precache-manifest.9a04ae232c5a6671ce307ee22695567b.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a447f0177997b77047c6d05f9ebf9325",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "57723de05a3a6da073d2",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d45820fdeca4c8067099.bundle.js"
+  },
+  {
+    "url": "runtime~main.d45820fdeca4c8067099.bundle.js"
+  },
+  {
+    "revision": "2cfb1822b540090517de",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.d45820fdeca4c8067099.bundle.js"
+  }
+]);
